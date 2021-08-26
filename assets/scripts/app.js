42493dbd6cfa5ae067a6b3279784245bfc510fc8
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2021-08-26 22:38:35 +0200

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,40 +1,42 @@
-const buildUrl = "Build";
-const loaderUrl = buildUrl + "/Build/Build.loader.js";
-const config = {
-  dataUrl: buildUrl + "/Build/Build.data",
-  frameworkUrl: buildUrl + "/Build/Build.framework.js",
-  codeUrl: buildUrl + "/Build/Build.wasm",
-  streamingAssetsUrl: "StreamingAssets",
-  companyName: "DefaultCompany",
-  productName: "Unity-Sammanslaget-2021",
-  productVersion: "1.0",
-};
+(function () {
+  const buildUrl = "Build";
+  const loaderUrl = buildUrl + "/Build/Build.loader.js";
+  const config = {
+    dataUrl: buildUrl + "/Build/Build.data",
+    frameworkUrl: buildUrl + "/Build/Build.framework.js",
+    codeUrl: buildUrl + "/Build/Build.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "DefaultCompany",
+    productName: "Unity-Sammanslaget-2021",
+    productVersion: "1.0",
+  };
 
-const container = document.querySelector("#unity-container");
-const canvas = document.querySelector("#unity-canvas");
-const loadingBar = document.querySelector("#unity-loading-bar");
-const progressBarFull = document.querySelector("#unity-progress-bar-full");
-const fullscreenButton = document.querySelector("#unity-fullscreen-button");
+  const container = document.querySelector("#unity-container");
+  const canvas = document.querySelector("#unity-canvas");
+  const loadingBar = document.querySelector("#unity-loading-bar");
+  const progressBarFull = document.querySelector("#unity-progress-bar-full");
+  const fullscreenButton = document.querySelector("#unity-fullscreen-button");
 
-canvas.style.width = "900px";
-canvas.style.height = "500px";
+  canvas.style.width = "900px";
+  canvas.style.height = "500px";
 
-loadingBar.style.display = "block";
+  loadingBar.style.display = "block";
 
-const script = document.createElement("script");
-script.src = loaderUrl;
-script.onload = () => {
-  createUnityInstance(canvas, config, (progress) => {
-    progressBarFull.style.width = 100 * progress + "%";
-  })
-    .then((unityInstance) => {
-      loadingBar.style.display = "none";
-      fullscreenButton.onclick = () => {
-        unityInstance.SetFullscreen(1);
-      };
+  const script = document.createElement("script");
+  script.src = loaderUrl;
+  script.onload = () => {
+    createUnityInstance(canvas, config, (progress) => {
+      progressBarFull.style.width = 100 * progress + "%";
     })
-    .catch((message) => {
-      alert(message);
-    });
-};
-document.body.appendChild(script);
+      .then((unityInstance) => {
+        loadingBar.style.display = "none";
+        fullscreenButton.onclick = () => {
+          unityInstance.SetFullscreen(1);
+        };
+      })
+      .catch((message) => {
+        alert(message);
+      });
+  };
+  document.body.appendChild(script);
+})();
